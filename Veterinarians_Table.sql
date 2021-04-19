677a74c7d3d492322a7986a6586bcc109f09b62c
KhuongVuong <50221077+KhuongVuong@users.noreply.github.com>
2021-04-19 13:07:03 -0500

    Add files via upload

---
+++ Veterinarians_Table.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Veterinarians (
+    vid int,
+    vName char(20),
+    PRIMARY KEY(vid)
+);
+-----------------------------------------
+INSERT INTO Veterinarians(vid, vName)
+VALUES(1001, John);
+INSERT INTO Veterinarians(vid, vName)
+VALUES(1002, David);
+INSERT INTO Veterinarians(vid, vName)
+VALUES(1003, Shara);
+INSERT INTO Veterinarians(vid, vName)
+VALUES(1004, Victor);
+INSERT INTO Veterinarians(vid, vName)
+VALUES(1005, Lora);
