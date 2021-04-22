be682f594beaf36b48a5b3cf1d4089942d39bd0f
KhuongVuong <50221077+KhuongVuong@users.noreply.github.com>
2021-04-22 18:25:33 -0500

    Update Homework4_GRANT.sql

--- Homework4_GRANT.sql
+++ Homework4_GRANT.sql
@@ -1,5 +1,23 @@
 ---CS411246---
 ----------------------------------------------
+--Create Veterinarians table--
+CREATE TABLE Veterinarians (
+    vid int,
+    vName char(20),
+    PRIMARY KEY(vid)
+);
+-----------------------------------------
+INSERT INTO Veterinarians(vid, vName)
+VALUES(1001, 'John');
+INSERT INTO Veterinarians(vid, vName)
+VALUES(1002, 'David');
+INSERT INTO Veterinarians(vid, vName)
+VALUES(1003, 'Shara');
+INSERT INTO Veterinarians(vid, vName)
+VALUES(1004, 'Victor');
+INSERT INTO Veterinarians(vid, vName)
+VALUES(1005, 'Lora');
+-----------------------------------------
 --Grant permission to cs411233--
 GRANT SELECT ON Veterinarians TO cs411233;
 GRANT REFERENCES(vid) ON Veterinarians TO cs411233;
