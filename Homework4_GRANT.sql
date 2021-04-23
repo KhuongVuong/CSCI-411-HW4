0da2e795a745460ec3baab89eec648e475d3bfa4
KhuongVuong <50221077+KhuongVuong@users.noreply.github.com>
2021-04-22 19:22:21 -0500

    Update Homework4_GRANT.sql

--- Homework4_GRANT.sql
+++ Homework4_GRANT.sql
@@ -29,12 +29,12 @@ GRANT INSERT ON Veterinarians TO cs411233;
 --Testing--
 --1.Find the person who between 40 and 50 by join Veterinarians with AGE table from cs411246--
 SELECT vName, vAge
-FROM Veterinarians, AGE.cs411246
+FROM Veterinarians, cs411233.AGE
 WHERE vAge BETWEEN 40 AND 50; 
 --2.Insert new values to AGE table in cs411233--
-INSERT INTO AGE.cs411233(vid, vAge) VALUES (1006, 52);
+INSERT INTO cs411233.AGE(vid, vAge) VALUES (1006, 52);
 --3.Update new values to AGE table in cs411233--
-UPDATE AGE.cs411233 SET vAge = '59' WHERE vid = 1006;
+UPDATE cs411233.AGE SET vAge = '59' WHERE vid = 1006;
 
 ----------------------------------------------
 ---CS411233---
@@ -44,7 +44,7 @@ CREATE TABLE AGE(
     vid int,
     vAge int,
     PRIMARY KEY(vid),
-    FOREIGN KEY(vid) REFERENCES Veterinarians.cs411246
+    FOREIGN KEY(vid) REFERENCES cs411246.Veterinarians
 );
 -------------------------------------------
 INSERT INTO AGE(vid, vAge) VALUES(1001, 40);
@@ -56,7 +56,7 @@ INSERT INTO AGE(vid, vAge) VALUES(1005, 51);
 --Create View from table Veterinarians in cs411246---
 CREATE OR REPLACE VIEW Vet_View
 AS SELECT vid, vName
-FROM Veterinarians.cs411246;
+FROM cs411246.Veterinarians;
 ----------------------------------------------
 ---Grant permission to cs411247---
 --1.Grant permission to insert and update VIEW(Vet_View)--
@@ -67,11 +67,11 @@ GRANT ALL ON AGE TO cs411247 WITH GRANT OPTION;
 --Testing--
 --1.Find the person name Lora and age by join AGE table with Veterinarians table from cs411246--
 SELECT vName, vAge
-FROM Veterinarians.cs411246, AGE
+FROM cs411246.Veterinarians, AGE
 WHERE Verterinarians.vid = AGE.vid
 AND vName = 'Lora';
 --2.Insert new values into Veterinarians table in cs411246--
-INSERT INTO Veterinarians.cs411246(vid, vName) VALUES (1007, 'Catherin');
+INSERT INTO cs411246.Veterinarians(vid, vName) VALUES (1007, 'Catherin');
 
 ----------------------------------------------
 ---CS411247---
@@ -81,10 +81,10 @@ GRANT SELECT, INSERT, UPDATE ON AGE.cs411233 TO cs411246;
 ----------------------------------------------
 --Testing--
 --1.Insert new values into VIEW (Vet_View table) from cs411233--
-INSERT INTO Vet_View.cs411233(vid, vName) VALUES (1006, 'Terisa');
+INSERT INTO cs411246.Vet_View(vid, vName) VALUES (1006, 'Terisa');
 --2.Update a name from VIEW (Vet_View table) in cs411233--
-UPDATE Vet_View.cs411233 SET vName = 'David.J' WHERE vid = 1001;
+UPDATE cs411246.Vet_View SET vName = 'David.J' WHERE vid = 1001;
 
 --Display result (1 and 2)--
 SELECT *
-FROM Vet_View.cs411233;
+FROM cs411246.Vet_View;
